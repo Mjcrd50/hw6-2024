acedf9fe772d8b6119621965bcbd929cc667dd3a
Rachael Celeste Zuppke <143569401+Mjcrd50@users.noreply.github.com>
2024-04-14 17:34:53 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -11,6 +11,8 @@ window.addEventListener("load", function() {
 
 document.getElementById("play").addEventListener("click", function play() {
 	video.play();
+	let volume = document.getElementById("volume")
+	console.log(volume)
 	console.log("Play Video")
 });
 
@@ -31,12 +33,37 @@ document.getElementById("faster").addEventListener("click", function setPlaySpee
 	console.log("Speed is " + video.playbackRate);
 	  })
 
-document.getElementById("skip").addEventListener("click", function() {
-		console.log("Skip ahead");
-		console.log("Video current time is")
+document.getElementById("skip").addEventListener("click", function skip() {
+	video.currentTime += 10;
+	video.loop=true;
+	console.log("Skip ahead");
+	console.log("Video current time is " + video.currentTime)
 	})
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.getElementById("mute").addEventListener("click", function () {
+	if (video.muted === false) {
+		video.muted=true;
+		console.log("Muted");
+	}
 
+	else {
+		video.muted=false;
+		console.log("Unmuted");
+	}
+})
+
+document.getElementById("slider").addEventListener("change", function () {
+	let sliderValue = document.getElementById("slider").value;
+	video.volume = sliderValue / 100;
+	console.log(sliderValue + "%");
+	})
+
+document.getElementById("vintage").addEventListener("click", function () {
+	video.classList.add("oldSchool")
+	console.log("Old School");
+	})
+
+document.getElementById("orig").addEventListener("click", function () {
+	video.classList.remove("oldSchool")
+	console.log("Original");
+	})
